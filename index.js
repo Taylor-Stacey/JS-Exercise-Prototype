d0a10786b99e6b9cc7b0200dbb61a5d999f3055c
Taylor <Taylor88stacey@gmail.com>
2022-02-16 14:51:50 -0800

    done

--- index.js
+++ index.js
@@ -39,14 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(someFood) {
+if(this.stomach.length < 10){
+  this.stomach.push(someFood)
+}
+}
 
-
-
-
+Person.prototype.poop = function() {
+  this.stomach = []
+}
+Person.prototype.toString = function() {
+  return `${this.name},${this.age}`
+}
 
 
 /*
@@ -63,8 +73,14 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons) {
+this.tank +=  gallons
 }
 
 
@@ -75,18 +91,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype)
 
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. global- when in the global scope, "this" will refer to the window/console object
+  2. new- "this" will refer to an item with in the object that is created and returned by the constructor function.
+  3. implicit- Whenever a preceding dot calls a function, the object before the dot is this
+  4. explicit- Whenever we use JavaScript's call or apply method, this is defined.
 */
 
 
